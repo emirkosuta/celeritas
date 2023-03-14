82a2cf656c37710ca622995ad43b4b42b8350a36
= <=>
2023-03-14 23:47:39 +0100

    add chi routes and serve web app

--- celeritas.go
+++ celeritas.go
@@ -3,9 +3,12 @@ package celeritas
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -18,6 +21,7 @@ type Celeritas struct {
 	ErrorLog *log.Logger
 	InfoLog  *log.Logger
 	RootPath string
+	Routes   *chi.Mux
 	config   config
 }
 
@@ -53,6 +57,7 @@ func (c *Celeritas) New(rootPath string) error {
 	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
 	c.Version = version
 	c.RootPath = rootPath
+	c.Routes = c.routes().(*chi.Mux)
 
 	c.config = config{
 		port:     os.Getenv("PORT"),
@@ -71,6 +76,22 @@ func (c *Celeritas) Init(p initPaths) error {
 		}
 	}
 	return nil
+}
+
+// start the webserver
+func (c *Celeritas) ListenAndServe() {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%s", os.Getenv("PORT")),
+		ErrorLog:     c.ErrorLog,
+		Handler:      c.routes(),
+		IdleTimeout:  30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 600 * time.Second,
+	}
+
+	c.InfoLog.Printf("Listening on port %s", os.Getenv("PORT"))
+	err := srv.ListenAndServe()
+	c.ErrorLog.Fatal(err)
 }
 
 func (c *Celeritas) checkDotEnv(path string) error {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/emirkosuta/celeritas
 
 go 1.19
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/joho/godotenv v1.5.1 // indirect
+)
