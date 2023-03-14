327173af5641afd3f2170c1a5a9230623d9d5bb9
= <=>
2023-03-14 22:57:30 +0100

    create folders and read env

---
+++ celeritas.go
@@ -0,0 +1,58 @@
+package celeritas
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+const version = "1.0.0"
+
+type Celeritas struct {
+	AppName string
+	Debug   bool
+	Version string
+}
+
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+
+	err := c.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+
+	err = c.checkDotEnv(rootPath)
+	if err != nil {
+		return err
+	}
+
+	err = godotenv.Load(rootPath + "/.env")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Celeritas) checkDotEnv(path string) error {
+	err := c.CreateFileIfNotExist(fmt.Sprintf("%s/.env", path))
+	if err != nil {
+		return err
+	}
+	return nil
+}
